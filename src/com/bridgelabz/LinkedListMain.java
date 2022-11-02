0cc499c050c79f3e96d133d5ce24f980bafcff64
adityabhartib <adityabharti.ab@gmail.com>
2022-11-02 22:40:56 +0530

    Added :UC3 Ability to Append Linked List

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -9,5 +9,11 @@ public class LinkedListMain {
         list.push(56);
         list.show();
 
+        LinkedList<Integer>list1=new LinkedList<>();
+        list1.add(56);
+        list1.add(30);
+        list1.add(70);
+        list1.show();
+
     }
 }
