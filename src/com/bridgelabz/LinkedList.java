0152b39eca1159cd07801616944510e07661e9b1
adityabhartib <adityabharti.ab@gmail.com>
2022-11-02 21:44:24 +0530

    Added : UC1 create simple linked List

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -4,14 +4,14 @@ public class LinkedList<T> {
     public Node<T> head;
     public Node<T> tail;
 
-    public void push (T data){
-        Node<T> node=new Node<>();
-        if (head==null){
-            head=node;
-            tail=node;
-        }else {
+    public void push(T data) {
+        Node<T> node = new Node<>();
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
             node.setNext(head);
-            head=node;
+            head = node;
         }
     }
 }
