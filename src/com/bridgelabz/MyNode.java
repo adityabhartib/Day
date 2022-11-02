ffdcb7d223e51645212636876ad1d7917bfb3be6
adityabhartib <adityabharti.ab@gmail.com>
2022-11-02 09:27:30 +0530

    Added : ability to add in Linked List

--- src/com/bridgelabz/MyNode.java
+++ src/com/bridgelabz/MyNode.java
@@ -2,27 +2,19 @@ package com.bridgelabz;
 
 public class MyNode<T> {
     private T key;
-
-    // K,V
-
     private MyNode<T> next;
-
     public MyNode(T key) {
         this.key = key;
     }
-
     public T getKey() {
         return key;
     }
-
     public void setKey(T key) {
         this.key = key;
     }
-
     public MyNode<T> getNext() {
         return next;
     }
-
     public void setNext(MyNode<T> next) {
         this.next = next;
     }
