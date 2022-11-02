cbb0e1bd459fac419ab5461109f7e9f6a4adba62
adityabhartib <adityabharti.ab@gmail.com>
2022-11-02 10:41:23 +0530

    Added : welcome msg

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,12 +3,5 @@ package com.bridgelabz;
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("-----Welcome to DataStructure Program-----");
-        LinkedList<Integer> list1 = new LinkedList();
-        list1.push(70);
-        list1.push(30);
-        list1.push(56);
-
-        list1.show();
-
     }
 }
